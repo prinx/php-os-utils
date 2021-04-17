f550f0547fc78bf6a0df845b528a5ac657277cd1
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-17 01:23:15 +0000

    Wrote tests

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -3,13 +3,26 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use Prinx\Os;
 
-/**
- * @todo The tests need works
- */
 class EnvTest extends TestCase
 {
-    public function testExample()
+    public function testSlash()
     {
+        $this->assertEquals(Os::slash(), DIRECTORY_SEPARATOR);
+        $this->assertEquals(Os::slash('win'), '\\');
+        $this->assertEquals(Os::slash('windows'), '\\');
+        $this->assertEquals(Os::slash('linux'), '/');
+        $this->assertEquals(Os::slash('mac'), '/');
+        $this->assertEquals(Os::slash('forward'), '/');
+        $this->assertEquals(Os::slash('backward'), '/');
+    }
+
+    public function testPlatformPathStyle()
+    {
+        $withForward = '/home/docs/codes';
+        $withBackward = '\\home\\docs\\codes';
+        $this->assertEquals(Os::toPathStyle($withForward, 'win'), $withBackward);
+        $this->assertEquals(Os::toPathStyle($withBackward, 'linux'), $withForward);
     }
 }
